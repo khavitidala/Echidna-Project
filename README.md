344b797153eb4d6eb33266f06565159d2702c779
Ryan Abdurohman <ryanabdurohman81@gmail.com>
2020-12-02 12:10:57 +0700

    mentahan

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # What is Echidna Project?
 Echidna project is an app to visualize selection and insertion sorting algorithm. The project aim is to fulfill an assignment project of Algorithm Complexity Analysis Course. This app using pygame module on python.
+
+1301190470 MOHAMMAD RIFQI ATIKO 
+1301191171 RYAN ABDUROHMAN 
---
+++ main.py
@@ -0,0 +1,36 @@
+import sys
+import pygame
+import time
+
+class Echidna:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((960,720))
+        pygame.display.set_caption("Selection and Insertion Sorting Algorithm")
+    
+    def run_app(self):
+
+    def _check_events(self):
+    
+    def _update_screen(self):
+
+if __name__ == '__main__':
+    echidna = Echidna()
+    echidna.run_app()
+
+# Function to do insertion sort 
+def insertionSort(arr): 
+  
+    # Traverse through 1 to len(arr) 
+    for i in range(1, len(arr)): 
+  
+        key = arr[i] 
+  
+        # Move elements of arr[0..i-1], that are 
+        # greater than key, to one position ahead 
+        # of their current position 
+        j = i-1
+        while j >= 0 and key < arr[j] : 
+                arr[j + 1] = arr[j] 
+                j -= 1
+        arr[j + 1] = key 
