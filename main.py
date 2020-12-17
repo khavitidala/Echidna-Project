7b9bf17b095e85f3cd86119eb6298e0ababb380f
Ryan Abdurohman <ryanabdurohman81@gmail.com>
2020-12-17 19:28:05 +0700

    final

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import pygame
 import time
 import random
-
+import threading
 class Button:
     def __init__(self, sortApp, text, width, height, position, color, font):
         self.screen = sortApp.screen
@@ -54,9 +54,26 @@ class Sort_screen:
         self.rect_surf1 = self.create_rect()
         self.screen.blit(self.rect_surf1, (self.x , self.y))
 
+class SortSprite(pygame.sprite.Sprite):
+    def __init__(self, dictRect):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.surface.Surface((dictRect["width"], dictRect["height"]))
+        self.image.fill(dictRect["color"])
+        self.rect = self.image.get_rect()
+        self.rect.x = dictRect["posX"]
+        self.rect.y = dictRect["posY"]
+    
+    def update(self, key, value):
+        if key == "color":
+            self.image.fill(value)
+        elif key == "width":
+            self.rect.width = value
+
+
 class Sort_Object:
     def __init__(self, sortApp, position, color, maxwidth, maxheight):
         self.screen = sortApp.screen
+        self.nilaimax = sortApp.nilaimax
         self.start_x = position[0]
         self.start_y = position[1]
         self.color = color
@@ -64,12 +81,13 @@ class Sort_Object:
         self.maxheight = float(maxheight-3)
         self.list_object = self.generate_list_of_rect()
         self.list_of_rect = self.generate_rect()
+        
 
     def generate_list_of_rect(self):
         self.list_object = []
         random.seed(5)
-        partHeight = self.maxheight/100
-        for i in range(0, 101):
+        partHeight = self.maxheight/self.nilaimax
+        for i in range(0, self.nilaimax):
             partWidth = random.randint(2, self.maxwidth-1)
             if i == 0:
                 posX = float(self.start_x)
@@ -82,100 +100,133 @@ class Sort_Object:
     def generate_rect(self):
         self.list_of_rect = []
         for i in self.list_object:
-            self.rect = pygame.Rect(0, 0, i["width"], i["height"])
-            self.rect.x = i["posX"]
-            self.rect.y = i["posY"]
-            self.list_of_rect.append(self.rect)
+            sprite = SortSprite(i)
+            sgrup = pygame.sprite.RenderUpdates(sprite)
+            self.list_of_rect.append(sgrup)
         return self.list_of_rect
             
     def draw_sort(self):
-        for i in range(len(self.list_object)):
-            pygame.draw.rect(self.screen, self.list_object[i]["color"], self.list_of_rect[i])
-    
-    #self.screen.fill(i["color"], self.rect)
+        for i in self.list_of_rect:
+            i.draw(self.screen)
 
-class Sorting:
+class Selection(pygame.sprite.Sprite):
     def __init__(self, sortApp, defaultColor):
+        pygame.sprite.Sprite.__init__(self)
         self.sortApp = sortApp
+        self.running_time = 0.0
         self.color = (0, 0, 0)
+        self.image = pygame.surface.Surface((320, 500))
+        self.rect = self.image.get_rect()
+        self.rect.x = 25
+        self.rect.y = 85
         self.bg_color = sortApp.bg_color
         self.screen = sortApp.screen
         self.sel_listObject = sortApp.sel_list
-        self.ins_listObject = sortApp.ins_list
         self.sel_listOfRect = sortApp.sel_rect
-        self.ins_listOfRect = sortApp.ins_rect
         self.defaultColor = defaultColor
+        self.surf = pygame.Surface((320, 500), pygame.SRCALPHA)
 
     def sel_draw_obj(self, index):
-        self.rect = pygame.Rect(0, 0, self.sel_listObject[index]["width"], self.sel_listObject[index]["height"])
-        self.rect.x = self.sel_listObject[index]["posX"]
-        self.rect.y = self.sel_listObject[index]["posY"]
-        self.sortApp._reset_screen_without_sortObj()
-        self.sel_listOfRect[index] = self.rect
-        for i in range(len(self.sel_listObject)):
-            pygame.draw.rect(self.screen, self.sel_listObject[i]["color"], self.sel_listOfRect[i])
-        self.change_rect2 = pygame.Rect(20, 80, 320, 500)
-        pygame.display.update(self.change_rect2)
+        
+        temp_s = SortSprite(self.sel_listObject[index])
+        self.sel_listOfRect[index].empty()
+        self.sel_listOfRect[index].add(temp_s)
+        self.change_rect2 = pygame.Rect(25, 85, 325, 505)
+        pygame.draw.rect(self.surf,self.bg_color, self.surf.get_rect())
+        self.screen.blit(self.surf, (self.rect.x, self.rect.y))
+        self.temp_group = pygame.sprite.RenderUpdates()
+        
+        for i in self.sel_listOfRect:
+            self.temp_group.add(i)
+        self.temp_group.draw(self.screen)
 
-    def selection_sort(self):
+    
+    def update(self):
+        start = time.perf_counter()
         self.size = len(self.sel_listObject)
         for step in range(self.size):
             min_idx = step
-            self.sel_listObject[step]["color"] = (0, 0, 0)
-            self.sel_draw_obj(step)
+            #self.sel_listObject[step]["color"] = (0, 0, 0)
+            #self.sel_draw_obj(step)
             for i in range(step + 1, self.size):
-                self.sel_listObject[i]["color"] = (255, 255, 255)
-                self.sel_draw_obj(i)
-                self.sel_listObject[min_idx]["color"] = (0, 0, 0)
-                self.sel_draw_obj(min_idx)
+                #self.sel_listObject[i]["color"] = (255, 255, 255)
+                #self.sel_draw_obj(i)
+                #self.sel_listObject[min_idx]["color"] = (0, 0, 0)
+                #self.sel_draw_obj(min_idx)
+                #self.sel_listOfRect[min_idx].update("color", (0, 0, 0))
                 if self.sel_listObject[i]["width"] < self.sel_listObject[min_idx]["width"]:
-                    self.sel_listObject[min_idx]["color"] = self.defaultColor
-                    self.sel_draw_obj(min_idx)
+                    #self.sel_listObject[min_idx]["color"] = self.defaultColor
+                    #self.sel_draw_obj(min_idx)
                     min_idx = i
-                    self.sel_listObject[min_idx]["color"] = (0, 0, 0)
-                    self.sel_draw_obj(min_idx)
-                self.sel_listObject[i]["color"] = self.defaultColor
+                    #self.sel_listObject[min_idx]["color"] = (0, 0, 0)
+                    #self.sel_draw_obj(min_idx)
+                #self.sel_listObject[i]["color"] = self.defaultColor
                 self.sel_draw_obj(i)
-            temp_index = step
+            #temp_index = step
             t = self.sel_listObject[step]["width"]
             self.sel_listObject[step]["width"] = self.sel_listObject[min_idx]["width"]
             self.sel_listObject[min_idx]["width"] = t
             self.sel_draw_obj(step)
             self.sel_draw_obj(min_idx)
-            self.sel_listObject[temp_index]["color"] = self.defaultColor
-            self.sel_draw_obj(temp_index)
-            
-    
-    def ins_draw_obj(self, index):
-        self.rect = pygame.Rect(0, 0, self.ins_listObject[index]["width"], self.ins_listObject[index]["height"])
-        self.rect.x = self.ins_listObject[index]["posX"]
-        self.rect.y = self.ins_listObject[index]["posY"]
-        self.sortApp._reset_screen_without_sortObj()
-        self.ins_listOfRect[index] = self.rect
-        for i in range(len(self.sel_listObject)):
-            pygame.draw.rect(self.screen, self.ins_listObject[i]["color"], self.ins_listOfRect[i])
-        self.change_rect = pygame.Rect(self.sortApp.ins_dinst, 80, 320, 500)
-        pygame.display.update(self.change_rect)
+            #self.sel_listObject[temp_index]["color"] = self.defaultColor
+            #self.sel_draw_obj(temp_index)
+        finish = time.perf_counter()
+        self.running_time = round(finish-start, 10)
+        print("Running time selection sort:", self.running_time)
 
-    def insertionSort(self):
+class Insertion(pygame.sprite.Sprite):
+    def __init__(self, sortApp, defaultColor):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.surface.Surface((325, 505))
+        self.running_time = 0.0
+        self.rect = self.image.get_rect()
+        self.rect.x = sortApp.ins_dinst+5
+        self.rect.y = 85
+        self.sortApp = sortApp
+        self.color = (0, 0, 0)
+        self.bg_color = sortApp.bg_color
+        self.screen = sortApp.screen
+        self.ins_listObject = sortApp.ins_list
+        self.ins_listOfRect = sortApp.ins_rect
+        self.defaultColor = defaultColor
+        self.surf = pygame.Surface((320, 500), pygame.SRCALPHA)
+
+    def ins_draw_obj(self, index):
+        temp_s = SortSprite(self.ins_listObject[index])
+        self.ins_listOfRect[index].empty()
+        self.ins_listOfRect[index].add(temp_s)
+        self.change_rect = pygame.Rect(self.sortApp.ins_dinst+5, 85, 325, 505)
+        pygame.draw.rect(self.surf,self.bg_color, self.surf.get_rect())
+        self.screen.blit(self.surf, (self.rect.x, self.rect.y))
+        self.temp_group = pygame.sprite.RenderUpdates()
+        for i in self.ins_listOfRect:
+            self.temp_group.add(i)
+        self.temp_group.draw(self.screen)
+
+    def update(self):
+        start = time.perf_counter()
         self.size = len(self.ins_listObject)  
         for i in range(1, self.size): 
             key = self.ins_listObject[i]["width"] 
-            self.ins_listObject[i]["color"] = (0, 0, 0)
-            self.ins_draw_obj(i)
+            #self.ins_listObject[i]["color"] = (0, 0, 0)
+            #self.ins_draw_obj(i)
             j = i-1
             while j >= 0 and key < self.ins_listObject[j]["width"]  :
-                    self.ins_listObject[j]["color"] = (0, 0, 0)
-                    self.ins_draw_obj(i) 
+                    #self.ins_listObject[j]["color"] = (0, 0, 0)
+                    #self.ins_draw_obj(i) 
                     self.ins_listObject[j + 1]["width"] = self.ins_listObject[j]["width"]
                     self.ins_draw_obj(j+1)
-                    self.ins_listObject[j]["color"] = self.defaultColor
-                    self.ins_draw_obj(i) 
+                    #self.ins_listObject[j]["color"] = self.defaultColor
+                    #self.ins_draw_obj(i) 
                     j -= 1
-            self.ins_listObject[i]["color"] = self.defaultColor
+            #self.ins_listObject[i]["color"] = self.defaultColor
             self.ins_draw_obj(i)
             self.ins_listObject[j + 1]["width"] = key
             self.ins_draw_obj(j+1)
+        finish = time.perf_counter()
+        self.running_time = round(finish-start, 10)
+        print("Running time insertion sort:", self.running_time)
+        
             
 
 class Echidna:
@@ -191,17 +242,24 @@ class Echidna:
         self.ins_dinst = 30+self.selection_screen.width+20
         self.insertion_screen = Sort_screen(self, 320, 500, 5, self.bg_color, (0, 0, 0), (self.ins_dinst, 80))
         self.ins_surf = self.insertion_screen.surf.get_rect()
+        self.nilaimax = 100
         self.selection_obj = Sort_Object(self, (25, 85), (4, 133, 253), self.selection_screen.width, self.selection_screen.height)
         self.insertion_obj = Sort_Object(self, (35+self.selection_screen.width+20, 85), (4, 133, 253), self.insertion_screen.width, self.insertion_screen.height)
         self.sel_rect = self.selection_obj.list_of_rect
         self.ins_rect = self.insertion_obj.list_of_rect
         self.sel_list = self.selection_obj.list_object
         self.ins_list = self.insertion_obj.list_object
-        self.sel_sorting = Sorting(self, (4, 133, 253))
+        self.sel_sorting = Selection(self, (4, 133, 253))
+        self.ins_sorting = Insertion(self, (4, 133, 253))
+        self.spriteSort = pygame.sprite.RenderUpdates(self.sel_sorting)
+        self.spriteSort2 = pygame.sprite.RenderUpdates(self.ins_sorting)
+        self.clock = pygame.time.Clock()
         #button
         self.sort_button = Button(self, "Sort", 200, 50, (self.screen.get_rect().centerx-100,self.height-10-80), (0, 0, 0), ("Arial", 48, (255, 255, 255)))
         self.sel_title = Button(self, "Selection", 200, 50, (75,25), self.bg_color, ("Arial", 32, (0, 0, 0)))
         self.ins_title = Button(self, "Insertion", 200, 50, (self.selection_screen.width+30+75,25), self.bg_color, ("Arial", 32, (0, 0, 0)))
+        self.runtime_sel = Button(self, "Running time: "+str(self.sel_sorting.running_time),200, 50,  (self.screen.get_rect().centerx-100,self.height-10-100), self.bg_color, ("Arial", 16, (0, 0, 0)))
+        self.runtime_ins = Button(self, "Running time: "+str(self.ins_sorting.running_time),200, 50,  (self.screen.get_rect().centerx+100,self.height-10-100), self.bg_color, ("Arial", 16, (0,0,0)))
 
     def run_app(self):
         while True:
@@ -217,8 +275,13 @@ class Echidna:
                 mouse_pos = pygame.mouse.get_pos()
                 button_clicked = self.sort_button.rect.collidepoint(mouse_pos)
                 if button_clicked:
-                    self.sel_sorting.selection_sort()
-                    self.sel_sorting.insertionSort()
+                    self.spriteSort.draw(self.screen)
+                    self.spriteSort2.draw(self.screen)
+                    t1 = threading.Thread(target=self.spriteSort.update)
+                    t2 = threading.Thread(target=self.spriteSort2.update)
+                    t1.start()
+                    t2.start()
+
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
                     self.sel_sorting.selection_sort()
@@ -234,6 +297,7 @@ class Echidna:
         self.sort_button.draw_button()
         self.sel_title.draw_button()
         self.ins_title.draw_button()
+        
 
     def _update_screen(self):
         self._reset_screen_without_sortObj()
